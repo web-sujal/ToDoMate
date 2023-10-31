7e7002b0d2b0c11956fe2b57e40dabc71fd56094
Sujal <web.sujal@gmail.com>
2023-10-31 12:41:07 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return(<>
+    <Typography variant='h2'>Hello World</Typography>
+  </>)
+}
+
+export default App
