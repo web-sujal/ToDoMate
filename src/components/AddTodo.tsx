d5989643a0b4ccba9df3c2afe3097c4b2a9e971c
Sujal <web.sujal@gmail.com>
2023-11-26 15:57:23 +0530

    removed form tag

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -11,11 +11,10 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import AddTags from "./AddTags";
 import { db } from "../config/firebase";
 import { Timestamp, addDoc, collection } from "firebase/firestore";
-import { useForm, SubmitHandler } from "react-hook-form";
 
 type AddTodoProps = {
   open: boolean;
@@ -25,7 +24,6 @@ type AddTodoProps = {
 const AddTodo = ({ open, setOpen }: AddTodoProps) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const { register, handleSubmit } = useForm();
 
   // states
   const [isAddTagsOpen, setIsAddTagsOpen] = useState(false);
@@ -34,9 +32,7 @@ const AddTodo = ({ open, setOpen }: AddTodoProps) => {
   const [tags, setTags] = useState<string[]>([]);
 
   // event handlers
-  const onSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-
+  const handleClick = async () => {
     try {
       await addDoc(collection(db, "todos"), {
         title,
@@ -86,125 +82,124 @@ const AddTodo = ({ open, setOpen }: AddTodoProps) => {
           Add Todo
         </Typography>
 
-        <form action="#" onSubmit={handleSubmit(onSubmit)}>
-          {/* title */}
-          <Typography sx={{ mb: 1 }}>Title</Typography>
-          <Input
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-            sx={{
-              mb: 2,
-              border: "1px solid",
-              borderColor: "secondary.contrastText",
-              borderRadius: 1,
-              px: 1,
-            }}
-            disableUnderline
-            placeholder="enter title..."
-            fullWidth
-            required
-            name="todo-title"
-          />
+        {/* title */}
+        <Typography sx={{ mb: 1 }}>Title</Typography>
+        <Input
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          sx={{
+            mb: 2,
+            border: "1px solid",
+            borderColor: "secondary.contrastText",
+            borderRadius: 1,
+            px: 1,
+          }}
+          disableUnderline
+          placeholder="enter title..."
+          fullWidth
+          required
+          name="todo-title"
+        />
 
-          {/* description */}
-          <Typography sx={{ mb: 1 }}>Description</Typography>
-          <Input
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            sx={{
-              mb: 2,
-              border: "1px solid",
-              borderColor: "secondary.contrastText",
-              borderRadius: 1,
-              px: 1,
-              py: 0.5,
-            }}
-            disableUnderline
-            multiline
-            rows={3}
-            placeholder="enter description..."
-            fullWidth
-            name="todo-description"
-          />
+        {/* description */}
+        <Typography sx={{ mb: 1 }}>Description</Typography>
+        <Input
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          sx={{
+            mb: 2,
+            border: "1px solid",
+            borderColor: "secondary.contrastText",
+            borderRadius: 1,
+            px: 1,
+            py: 0.5,
+          }}
+          disableUnderline
+          multiline
+          rows={3}
+          placeholder="enter description..."
+          fullWidth
+          name="todo-description"
+        />
 
-          {/* Tags */}
+        {/* Tags */}
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          gap={1}
+          sx={{ mb: 2 }}
+        >
           <Stack
             direction="row"
             alignItems="center"
+            flexWrap="wrap"
             justifyContent="space-between"
             gap={1}
-            sx={{ mb: 2 }}
           >
-            <Stack
-              direction="row"
-              alignItems="center"
-              justifyContent="space-between"
-              gap={1}
-            >
-              <Typography>Tags:</Typography>
-              {tags.length ? (
-                <Stack
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  gap={1}
-                >
-                  {tags.map((tag) => (
-                    <Chip
-                      key={tag}
-                      label={tag}
-                      variant="outlined"
-                      color="success"
-                      size="small"
-                      onDelete={() =>
-                        console.info("just clicked from addtodo modal tags")
-                      }
-                    />
-                  ))}
-                </Stack>
-              ) : (
-                <Typography sx={{ color: "rgba(0,0,0,0.8)" }}>
-                  (No Tags Selected)
-                </Typography>
-              )}
-            </Stack>
-
-            <AddCircle
-              onClick={() => setIsAddTagsOpen(true)}
-              sx={{
-                cursor: "pointer",
-                color: "secondary.contrastText",
-                "&:hover": {
-                  transform: "translateY(-3px) scale(1.05)",
-                  transition: "transform 0.3s",
-                },
-              }}
-            />
-
-            {/* tags modal */}
-            <AddTags
-              isAddTagsOpen={isAddTagsOpen}
-              setIsAddTagsOpen={setIsAddTagsOpen}
-            />
+            <Typography>Tags:</Typography>
+            {tags.length ? (
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                gap={1}
+              >
+                {tags.map((tag) => (
+                  <Chip
+                    key={tag}
+                    label={tag}
+                    variant="outlined"
+                    color="success"
+                    size="small"
+                    onDelete={() =>
+                      console.info("just clicked from addtodo modal tags")
+                    }
+                  />
+                ))}
+              </Stack>
+            ) : (
+              <Typography sx={{ color: "rgba(0,0,0,0.8)" }}>
+                (No Tags Selected)
+              </Typography>
+            )}
           </Stack>
 
-          {/* buttons */}
-          <ButtonGroup
-            variant="outlined"
-            color="primary"
-            aria-label="add todo button"
-            fullWidth
-            sx={{ mb: 2 }}
+          <AddCircle
+            onClick={() => setIsAddTagsOpen(true)}
+            sx={{
+              cursor: "pointer",
+              color: "secondary.contrastText",
+              "&:hover": {
+                transform: "translateY(-3px) scale(1.05)",
+                transition: "transform 0.3s",
+              },
+            }}
+          />
+
+          {/* tags modal */}
+          <AddTags
+            isAddTagsOpen={isAddTagsOpen}
+            setIsAddTagsOpen={setIsAddTagsOpen}
+          />
+        </Stack>
+
+        {/* buttons */}
+        <ButtonGroup
+          variant="outlined"
+          color="primary"
+          aria-label="add todo button"
+          fullWidth
+          sx={{ mb: 2 }}
+        >
+          <Button onClick={handleClick}>Add</Button>
+          <Button
+            onClick={() => setOpen(false)}
+            sx={{ maxWidth: "100px", color: "error" }}
           >
-            <Button type="submit">Add</Button>
-            <Button
-              onClick={() => setOpen(false)}
-              sx={{ maxWidth: "100px", color: "error" }}
-            >
-              Cancel
-            </Button>
-          </ButtonGroup>
-        </form>
+            Cancel
+          </Button>
+        </ButtonGroup>
       </Box>
     </Modal>
   );
