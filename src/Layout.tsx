8d48c6a14dc4a48b681d7c5c4c2348ccf35b62e1
Sujal <web.sujal@gmail.com>
2023-11-27 19:59:47 +0530

    modified layout page

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,14 +2,13 @@ import { Box, Stack } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import "./App.css";
-import { Navigate, useLocation, useNavigate } from "react-router";
-import { auth } from "./config/firebase";
 import { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router";
 import { User } from "@firebase/auth";
+import { auth } from "./config/firebase";
 
 const Layout = () => {
   const [user, setUser] = useState<User | null>(null);
-  const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
