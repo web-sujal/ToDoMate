2e969b37e3fc8a26efcb1ac2613d8d1ebce0e760
Sujal <web.sujal@gmail.com>
2023-11-28 10:27:25 +0530

    working build - removed onAuthStateChanged form both Layout and PrivateRoutes to test on deployement server

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,6 +2,34 @@ import { Box, Stack } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import "./App.css";
+import { Navigate, useLocation } from "react-router-dom";
+import { auth } from "./config/firebase";
+
+const Layout = () => {
+  const user = auth.currentUser;
+  const location = useLocation();
+
+  if (location.pathname === "/")
+    return user ? <Navigate to="/overview" /> : <Navigate to="/login" />;
+
+  return (
+    <Stack direction="row">
+      <Box sx={{ display: { xs: "none", sm: "block", height: "100vh" } }}>
+        <Sidebar />
+      </Box>
+      <Home />
+    </Stack>
+  );
+};
+
+export default Layout;
+
+{
+  /*
+import { Box, Stack } from "@mui/material";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import "./App.css";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { auth } from "./config/firebase";
 import { useEffect } from "react";
@@ -39,25 +67,5 @@ const Layout = () => {
 };
 
 export default Layout;
-
-{
-  /*
-    const [user, setUser] = useState<User | null>(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const unsubscribe = () => {
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-          setUser(user);
-          navigate("/overview");
-        } else {
-          navigate("/login");
-        }
-      });
-    };
-
-    return () => unsubscribe();
-  }, []);
    */
 }
